f6839ac3c83ac391e55d8d4ffeb82d7ec4858358
typespt <65303033+typespt@users.noreply.github.com>
2021-05-19 23:40:02 +0300

    основная логика без заданий со звездочкой. метки goto не работают. Реванш не работает

--- Task2Box/Program.cs
+++ Task2Box/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Task2Box
 {
@@ -6,7 +7,51 @@ namespace Task2Box
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Введите имена игроков");
+            string[] players = new string[2];
+
+            for (int i = 0; i < players.GetLength(0); i++)
+            {
+                Console.Write($"Введите имя игрока №{i + 1} - ");
+                players[i] = Console.ReadLine();
+            }
+            Console.WriteLine("Таблица игроков:");
+            for (int i = 0; i < players.Length; i++)
+            {
+                Console.WriteLine($"Игрок №{i + 1} - " + players[i]);
+            }
+
+            Begin:
+
+            Random randomize = new Random();
+            int random = randomize.Next(12, 121);
+            Console.WriteLine();
+            Console.WriteLine();
+
+
+            Console.WriteLine("Загаданное число " + random);
+
+            while (random != 0)
+            {
+                for (int i = 0; i < players.GetLength(0); i++)
+                {
+                    Console.Write($"Ход игрока  {players[i]} - ");
+                    random = random - int.Parse(Console.ReadLine());
+                    Console.WriteLine(random);
+
+                    if (random == 0)
+                    {
+                        Console.WriteLine("Победил игрок " + players[i]);
+                        break;
+                    }                    
+                }
+            }
+            Console.WriteLine("Для повторной игры нажмите 1");
+            int option = Console.Read();
+            if (option == 1)
+            {
+                goto Begin;
+            }
         }
     }
 }
