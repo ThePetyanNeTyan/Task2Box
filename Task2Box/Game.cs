2c12c374864acf4e9d7c0f52ab00beaa5793478b
typespt <65303033+typespt@users.noreply.github.com>
2021-05-25 23:10:35 +0300

    перетащил игру в ООП вид

---
+++ Task2Box/Game.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task2Box
+{
+    class Game
+    {
+        private Player _playerOne;
+        private Player _playerTwo;
+        private Random _random = new Random();
+        private int _randomNumber;
+
+        public void Welcome()
+        {
+            Console.WriteLine("Для игры с человеком нажмите 1 против бота - любая цифра");
+            var isParse = int.TryParse(Console.ReadLine(), out var result);
+            if (isParse == false)
+                throw new Exception("Вы ввели не цифру");
+
+            if (result == 1)
+            {
+                CreatePlayers();
+                GenerateRandomNumber();
+                StartGame();
+            }
+            else
+            {
+                CreateComputerGame();
+                GenerateRandomNumber();
+                StartGame();
+            }
+        }
+        private void CreatePlayers()
+        {
+            Console.Write("Player1: ");
+            _playerOne = Player.CreatePlayer(Console.ReadLine());
+            Console.Write("Player2: ");
+            _playerTwo = Player.CreatePlayer(Console.ReadLine());
+        }
+
+        private void CreateComputerGame()
+        {
+            Console.Write("Player1: ");
+            _playerOne = Player.CreatePlayer(Console.ReadLine());
+            Console.Write("Player2: ");
+            _playerTwo = Player.CreateComputer();
+        }
+
+        private int GenerateRandomNumber()
+        {
+            _randomNumber = _random.Next(12, 121);
+            Console.WriteLine("Загаданное число " + _randomNumber);
+            return _randomNumber;
+        }
+
+        private void StartGame()
+        {
+            while (_randomNumber > 0)
+            {
+                    Console.Write($"Ход игрока  {_playerOne} - ");
+                    int chooseOne = _playerOne.MakeStep();
+                    _randomNumber = _randomNumber - chooseOne;
+                    Console.WriteLine(_randomNumber);
+                if (_randomNumber <= 0)
+                {
+                    Console.WriteLine("Победил игрок " + _playerOne.ToString());
+                    break;
+                }
+
+                    Console.Write($"Ход игрока  {_playerTwo} - ");
+                    int chooseTwo = _playerTwo.MakeStep();                  
+                    _randomNumber = _randomNumber - chooseTwo;
+                    Console.WriteLine(_randomNumber);
+                if (_randomNumber <= 0)
+                {
+                    Console.WriteLine("Победил игрок " + _playerTwo.ToString());
+                    break;
+                }
+            }
+        }
+    }
+}
---
+++ Task2Box/Player.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task2Box
+{
+    class Player
+    {
+        private Player(string name, bool isComputer)
+        {
+            Name = name;
+            IsComputer = isComputer;
+        }
+
+        public string Name { get; }
+        public bool IsComputer { get; }
+
+        public static Player CreatePlayer(string name)
+        {
+            return new Player(name, false);
+        }
+
+        public static Player CreateComputer()
+        {
+            return new Player("Компьютер", true);
+        }
+
+        public int MakeStep()
+        {
+            if (IsComputer)
+            {
+                Random rnd = new Random();
+                return rnd.Next(1, 5);
+            }
+            else
+            {
+                int i = 0;
+                while (i > 4 || i <= 0)
+                {
+                    Console.WriteLine("Введите цифры от 1 до 4");
+                    i = int.Parse(Console.ReadLine());
+                }
+                return i;
+            }
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
